46c287a396f04bd4276e9be733b262db60e16d7a
sagar <sagar@softvan.in>
2017-02-25 14:55:06 +1300

    Store model file into h5 python binary format and store model informatation into json file.

--- Radiant_Lab_ML/com/radiantlab/ml/script/tngScript.py
+++ Radiant_Lab_ML/com/radiantlab/ml/script/tngScript.py
@@ -7,10 +7,13 @@ from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 
-
 # create model function and configure input and output neurons (single fully connected hidden layer)
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+
+inputFilePath = "/home/sagar/RadiantLab_Data/ML_Data/monthlyCashFlow_100B.csv"
+modelPath = "/home/sagar/RadiantLab_Data/ML_Data/monthlyCashFlow_100B.h5";
+modelJsonPath = "/home/sagar/RadiantLab_Data/ML_Data/monthlyCashFlow_100B.json"
 
 
 def baseline_model():
@@ -22,11 +25,17 @@ def baseline_model():
     model.add(Dense(1, init='normal'))
     # Compile model with optimizer
     model.compile(loss='mean_squared_error', optimizer='adam')
+    #The h5py package is a Pythonic interface to the HDF5 binary data format.
+    model.save(modelPath)
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(modelJsonPath, "w") as json_file:
+        json_file.write(model_json)
     return model
 
 
 # load dataset from CSV into pandas object
-dataframe = pandas.read_csv("/home/sagar/RadiantLab_Data/ML_Data/monthlyCashFlow_100B.csv", sep=",", header=0)
+dataframe = pandas.read_csv(inputFilePath, sep=",", header=0)
 dataset = dataframe._values
 
 # split into input (X) and output (Y) variables
@@ -45,7 +54,7 @@ seed = 7
 numpy.random.seed(seed)
 
 # evaluate model with dataset passing with parameters
-estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=100, batch_size=5, verbose=0)
+estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=5, batch_size=5, verbose=0)
 
 # The final step is to evaluate this baseline model. We will use 10-fold cross validation to evaluate the mode
 kfold = KFold(n_splits=5, random_state=seed)
@@ -55,17 +64,13 @@ kfold = KFold(n_splits=5, random_state=seed)
 results = cross_val_score(estimator, X_Feature_Vector, Y_Output_Vector, cv=kfold)
 print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
 
-#Now scale the data and check its MSE Error
-#Evaluate model with standardized dataset these method scale data based on StandardScaler technique (sklearn.preprocessing.StandardScaler)
-numpy.random.seed(seed)
+# Now scale the data and check its MSE Error
+# Evaluate model with standardized dataset these method scale data based on StandardScaler technique (sklearn.preprocessing.StandardScaler)
 estimators = []
 estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp', KerasRegressor(build_fn=baseline_model, nb_epoch=50, batch_size=5, verbose=0)))
+estimators.append(('mlp', KerasRegressor(build_fn=baseline_model, nb_epoch=5, batch_size=5, verbose=0)))
 pipeline = Pipeline(estimators)
 kfold = KFold(n_splits=5, random_state=seed)
-
 results = cross_val_score(pipeline, X_Feature_Vector, Y_Output_Vector, cv=kfold)
-#Print the MSE for unseen data
+# Print the MSE for unseen data
 print("Standardized: %.2f (%.2f) MSE" % (results.mean(), results.std()))
-
-
